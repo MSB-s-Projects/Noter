4549a13c08895f19bf5d4c0dd23855f71ab6ca12
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-02-22 01:49:02 +0530

    add functionality to add notes

--- src/components/CreateArea.tsx
+++ src/components/CreateArea.tsx
@@ -1,4 +1,20 @@
-export default function CreateArea() {
+import { useState } from 'react'
+
+export default function CreateArea({ addNote }: { addNote: Function }) {
+	const [newNote, setNewNote] = useState({ title: '', content: '' })
+
+	function handelChange(event: { target: { name: string; value: string } }) {
+		const { name, value } = event.target
+
+		setNewNote((prevValue) => ({ ...prevValue, [name]: value }))
+	}
+
+	function handleClick(event: { preventDefault: () => void }) {
+		event.preventDefault()
+		addNote((prevValue: any) => [...prevValue, newNote])
+		setNewNote({ title: '', content: '' })
+	}
+
 	return (
 		<div className='card items-center'>
 			<div className='card w-96 bg-base-100 shadow-md shadow-primary'>
@@ -9,25 +25,26 @@ export default function CreateArea() {
 							type='text'
 							placeholder='Title'
 							className='input input-lg input-ghost w-full max-w-xs'
+							onChange={handelChange}
+							value={newNote.title}
 						/>
 						<textarea
 							name='content'
 							className='textarea textarea-lg textarea-ghost w-full max-w-xs'
 							placeholder='Take a note...'
-							rows={3}></textarea>
+							rows={3}
+							onChange={handelChange}
+							value={newNote.content}></textarea>
 						<div className='card-actions justify-end'>
-							<button className='btn btn-circle btn-primary absolute -bottom-5 right-8 text-2xl'>
+							<button
+								onClick={handleClick}
+								className='btn btn-circle btn-primary absolute -bottom-5 right-8 text-2xl'>
 								<span className='mb-1'>+</span>
 							</button>
 						</div>
 					</form>
 				</div>
 			</div>
-			{/* <form className=''>
-				<input name='title' placeholder='Title' />
-				<textarea name='content' placeholder='Take a note...' rows={3} />
-				<button>Add</button>
-			</form> */}
 		</div>
 	)
 }
