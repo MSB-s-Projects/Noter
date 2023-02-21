6fd03a46355dfa372b57cf3524b2663a79782907
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-02-21 19:48:30 +0530

    added styles to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
-export default function Header(){
-    return (
-        <header>
-            <h1>Noter</h1>
-        </header>
-    )
+export default function Header() {
+	return (
+		<header className='my-4 mx-2'>
+			<div className='navbar bg-primary text-primary-content rounded-lg'>
+				<a className='btn btn-ghost normal-case text-xl' href='/'>
+					NOTEr
+				</a>
+			</div>
+		</header>
+	)
 }
