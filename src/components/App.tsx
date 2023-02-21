c50654f03a24db43f5b2daf351679356c9a0316b
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-02-21 22:49:17 +0530

    add dark and light theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,15 @@
+import CreateArea from './CreateArea'
 import Footer from './Footer'
 import Header from './Header'
 
 export default function App() {
+
 	return (
-		<>
+		<div>
 			<Header />
+			{/* <CreateArea /> */}
 			<Footer />
-		</>
+		</div>
 	)
 }
+
